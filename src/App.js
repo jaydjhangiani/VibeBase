5b8c5d18ef26a5717f9fcea1cb90a80a26c23c66
Jay Jhangiani <47749517+jaydjhangiani@users.noreply.github.com>
2021-06-12 12:04:04 +0530

    Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Routes from "./utils/Routes";
+import { useAuthState } from "react-firebase-hooks/auth";
+import Loader from "./components/Loader";
+import Login from "./screens/LoginScreen";
+import { auth } from "./utils/firebase";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [user, loading] = useAuthState(auth);
+
+  if (loading) return <Loader />;
+
+  if (!user) return <Login />;
+
+  return <Routes />;
 }
 
 export default App;
